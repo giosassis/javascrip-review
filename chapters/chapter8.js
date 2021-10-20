b778e3e10977de3a8508677f3b252dd942b0a2b7
Giovana Sant Anna de Assis <CIANDT\giovanaa@lnb024128bhz.ciandt.global>
2021-10-20 10:01:15 -0300

    feat: chapter 8

--- chapters/chapter8.js
+++ chapters/chapter8.js
@@ -5,3 +5,26 @@ function canYouSpotTheProblem() {
         console.log("Happy happy!")
     }
 }
+
+//Tratamento de Erro//
+
+function promptDirection(question) {
+    let result = question 
+    if (result.toLowerCase() == "Left") return "L"
+    if (result.toLowerCase() == "Right") return "R"
+    else throw new Error("Invalid direction")
+}
+
+function look(){
+    if(promptDirection("Which direction do you want?") == "L") {
+        return "House"
+    } else {
+        return "Two angry bears"
+    }
+}
+
+try {
+    console.log("You see", look())
+} catch (error) {
+    console.log("Something went wrong", error)
+}

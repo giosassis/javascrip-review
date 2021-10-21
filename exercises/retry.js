deab466be25364abecd3aabec8fab4c47c763960
Giovana Sant Anna de Assis <CIANDT\giovanaa@lnb024128bhz.ciandt.global>
2021-10-21 16:52:22 -0300

    refactor: retry exercise

--- exercises/retry.js
+++ exercises/retry.js
@@ -1,3 +1,8 @@
 function primitiveMultiply(a, b) {
-    
+  try {
+    if (Math.random() < 0.2) return a * b;
+    throw new Error("Multiplication Failure");
+  } catch (error) {
+    console.error(error);
+  }
 }
